68520b2753d19b2dbaf955ec7db673cf72a0f06c
krams <mstaborda00@gmail.com>
2024-02-19 13:16:56 +0800

    feat: fixing error BE

--- server/server.js
+++ server/server.js
@@ -9,8 +9,19 @@ require("dotenv").config();
 
 const app = express();
 
-// Allow CORS for all origins
-app.use(cors());
+// // Allow CORS for all origins
+// app.use(cors());
+
+// Allow CORS for all origins and methods
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  next();
+});
 
 // Parse JSON request bodies
 app.use(bodyParser.json());
