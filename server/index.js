57a37ccab80b23d93e8b696b4b2eb1066d4c1eb9
krams <mstaborda00@gmail.com>
2024-02-19 13:55:18 +0800

    feat: minimal fixing at BE

--- server/index.js
+++ server/index.js
@@ -1,35 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const requestApiRoutes = require("./Request/routes/Api");
 const userApiRoutes = require("./User/routes/userApi");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-// Define allowCors middleware
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
-
-// Apply allowCors middleware to all routes
-app.use(allowCors);
 
 // Parse JSON request bodies
 app.use(bodyParser.json());
